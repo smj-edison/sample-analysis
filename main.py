b74614d5defa0822cce457fcdfded3d97c26ad6a
smj-edison <stephen.mason.jones@gmail.com>
2022-09-16 10:21:45 -0700

    Play a chord

--- main.py
+++ main.py
@@ -22,33 +22,42 @@ analysis_range = [420, 460]
 sample_rate, nontremmed = load_audio_mono("./069-A-nt.wav")
 sample_rate, tremmed = load_audio_mono("./069-A.wav")
 
+_, c_sharp = load_audio_mono("./test-samples/073-C#.wav")
+_, e = load_audio_mono("./test-samples/076-E.wav")
+
 nt_freq, nt_amp = get_sig_freq_and_amp(nontremmed, sample_rate, analysis_range[0], analysis_range[1])
 base_freq = np.mean(nt_freq)
 base_amp = np.mean(nt_amp)
 
-freq_trem_table, amp_trem_table = calc_trem_table(tremmed, 440, sample_rate, plot_results=True)
+freq_trem_table, amp_trem_table, trem_freq = calc_trem_table(tremmed, 440, sample_rate, plot_results=True)
+
+detune_trem_table = freq_trem_table / base_freq
+gain_trem_table = amp_trem_table / base_amp
+
+def audio_lookup(pos, audio):
+    sample0 = audio[floor(pos_in_audio - 1)]
+    sample1 = audio[floor(pos_in_audio + 0)]
+    sample2 = audio[floor(pos_in_audio + 1)]
+    sample3 = audio[floor(pos_in_audio + 2)]
+
+    return hermite_interp(sample0, sample1, sample2, sample3, pos % 1) * gain
 
 audio_out = []
-audio_loc = 0
+audio_loc = 1/sample_rate
 global_loc = 0
-table_freq = 6.3
 detune = 1
 
-while audio_loc < (len(nontremmed) - 4) / sample_rate:
+output_audio_len = np.min([len(nontremmed), len(c_sharp), len(e)]) - 4
+
+while audio_loc < output_audio_len / sample_rate:
     pos_in_audio = audio_loc * sample_rate
 
-    target_freq = table_lookup(freq_trem_table, table_freq, global_loc)
-    detune = target_freq / base_freq
+    detune = table_lookup(detune_trem_table, trem_freq, global_loc)
+    gain = table_lookup(gain_trem_table, trem_freq, global_loc) * 0.3
 
-    target_amp = table_lookup(amp_trem_table, table_freq, global_loc)
-    gain = target_amp / base_amp
-
-    sample0 = nontremmed[floor(pos_in_audio - 1)]
-    sample1 = nontremmed[floor(pos_in_audio + 0)]
-    sample2 = nontremmed[floor(pos_in_audio + 1)]
-    sample3 = nontremmed[floor(pos_in_audio + 2)]
-
-    sample = hermite_interp(sample0, sample1, sample2, sample3, pos_in_audio % 1) * gain
+    sample = (audio_lookup(pos_in_audio, nontremmed) * gain +
+              audio_lookup(pos_in_audio, c_sharp) * gain +
+              audio_lookup(pos_in_audio, e) * gain)
 
     audio_out.append(sample)
 
@@ -56,5 +65,4 @@ while audio_loc < (len(nontremmed) - 4) / sample_rate:
     audio_loc += detune / sample_rate
     global_loc += 1 / sample_rate
 
-wavfile.write("out.wav", sample_rate, np.array(audio_out))
-
+wavfile.write("out-chord.wav", sample_rate, np.array(audio_out))
