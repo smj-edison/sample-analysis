e64d529f4f922848b9bff210248bba54fddc701d
smj-edison <stephen.mason.jones@gmail.com>
2022-09-17 20:15:25 -0700

    Working envelope detector and looper

--- envelope.py
+++ envelope.py
@@ -10,9 +10,9 @@ from scipy.io import wavfile
 from helpers import butter_lowpass_filter, load_audio_mono, calc_rms, norm_sig, resample_to
 
 
-sample_rate, nontremmed = load_audio_mono("./test-samples/076-E.wav")
+sample_rate, nontremmed = load_audio_mono("./test-samples/036-C-nt.wav")
 nontremmed, source_min, source_max = norm_sig(nontremmed)
-freq = 659
+freq = 65.41
 
 # use the hilbert transform to get the envelope
 envelope = calc_amp(nontremmed)
@@ -47,7 +47,7 @@ release_index = -1
 # search for spots to stay within for picking a loop
 search_width = 40000
 search_step = 100
-strictness_start = 0.3
+strictness_start = 0.5
 strictness_relax_factor = 1.2
 too_far_in_percentage_attack = 0.15
 too_far_in_percentage_release = 0.2
@@ -80,7 +80,7 @@ while release_index == -1:
         rms = calc_rms(envelope_deriv[(i - search_width):i])
 
         if rms < (std * strictness) + mean:
-            release_index = i
+            release_index = i - search_width / 2
             break
 
     # if the loop point seems too far in the sample, keep iterating
@@ -107,39 +107,26 @@ highest_score = -math.inf
 highest_index = -1
 
 ref_sample = potential_loop[0:increment_by_int]
-ref_sample_amps = resample_to(abs(rfft(ref_sample)[1:]), increment_by)
+ref_sample_amps = resample_to(abs(rfft(ref_sample, norm="ortho")[1:]), increment_by_int)
+bias = (1 - (np.linspace(0.0, 1.0, increment_by_int) ** 2)) * 2
 
-last_plot = -math.inf
 
-for i in np.arange(len(potential_loop) * 0.6, len(potential_loop) - increment_by_int, 1):
+for i in np.arange(len(potential_loop) * 0.6, len(potential_loop) - increment_by_int, 2):
     pos = floor(i)
 
     # calculate score based on what provides the least harmonic distortion
     potential_end = potential_loop[(pos - increment_by_int):pos]
-    analysis = abs(rfft(np.concatenate((potential_end, ref_sample)))[1:]) / ref_sample_amps
-    bias = (1 - (np.linspace(0.0, 1.0, len(analysis)) ** 2)) * 2
+    analysis = abs(rfft(np.concatenate((potential_end, ref_sample)), norm="ortho")[1:]) / ref_sample_amps
     
-    score = -math.sqrt(np.sum(np.abs(analysis / ref_sample_amps * bias)**2))
+    score = -math.sqrt(np.sum(np.abs(analysis * bias)**2))
 
     if score > highest_score:
-        if score - last_plot > 20:
-            plt.plot(np.concatenate((potential_end, ref_sample)))
-            plt.show()
-
-            last_plot = score
-
         highest_score = score
         highest_index = pos
 
-        print(f"harmonic distortion: {-score}")
-
-plt.plot(np.concatenate((potential_loop[(highest_index - increment_by_int):highest_index], ref_sample)))
-plt.show()
-
 loop_end = attack_index + highest_index
 
-
-plt.plot(np.concatenate((nontremmed[(loop_end - 512):loop_end], nontremmed[attack_index:(attack_index + 512)])))
+plt.plot(np.concatenate((nontremmed[(loop_end - increment_by_int):loop_end], nontremmed[attack_index:(attack_index + increment_by_int)])))
 plt.show()
 
 # loop test
@@ -147,7 +134,7 @@ loop = nontremmed[attack_index:loop_end]
 out = np.concatenate((loop, loop, loop, loop, loop))
 wavfile.write("loop.wav", sample_rate, out)
 
-plt.plot(envelope_smoothed)
+plt.plot(envelope_deriv)
 plt.axvline(x=peak_attack, color="blue")
 plt.axvline(x=attack_index, color="green")
 plt.axvline(x=loop_end, color="cyan")
