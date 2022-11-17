00b93f9759b0f3520adf7bf6364f0344f3269206
smj-edison <stephen.mason.jones@gmail.com>
2022-11-17 15:31:02 -0800

    WIP

--- envelope.py
+++ envelope.py
@@ -107,99 +107,59 @@ attack_index += np.argmin(abs(search_area))
 # PART TWO: find loop point
 
 # VV alternate approach (WIP) VV
-# set slice size (align for FFT)
-# slice_width = 2 ** ceil(log2(max((sample_rate / freq), 512)))
+slice_width = max((sample_rate / freq), 512) // 1
 
-# # look for a spot of equal amplitude as attack
-# attack_amp = envelope_db[attack_index]
+# look for a spot of equal amplitude as attack
+attack_amp = envelope_db[attack_index]
 
-# loop_end_search_start = floor(len(nontremmed) * 0.6)
-# loop_end_area = np.argmin(abs(envelope_db[loop_end_search_start:release_index] - attack_amp)) + loop_end_search_start
+loop_end_search_start = floor(len(nontremmed) * 0.6)
+loop_end_area = np.argmin(abs(envelope_db[loop_end_search_start:release_index] - attack_amp)) + loop_end_search_start
 
-# ref_sample = nontremmed[attack_index:(attack_index + slice_width)]
-# ref_fft = zoom_fft(ref_sample, [freq / 2, freq * 16.5], m=512)
-# ref_phases = np.angle(ref_fft)
-
-# loop_end_sample = nontremmed[loop_end_area:(loop_end_area + slice_width)]
-# loop_end_fft = zoom_fft(loop_end_sample, [freq / 2, freq * 16], m=512)
-# loop_end_phases = np.angle(loop_end_fft)
-
-# phase_diff = (loop_end_phases[16] - ref_phases[16]) * freq / math.pi
-# loop_end = floor(loop_end_area + phase_diff)
-
-# plt.plot(np.concatenate((nontremmed[(loop_end - slice_width):loop_end],
-#                          nontremmed[attack_index:(attack_index + slice_width)])))
-# plt.show()
-
-# PART TWO: find loop point
-# I use a pretty unique solution here. I was inspired when I was finding loops manually.
-# The way I determined whether a loop was good or not was whether it made a "click" sound
-# when it looped. But, what is a click sound but harmonic distortion?
-
-# My algorithm tries every possible loop, and calculates the harmonic distortion
-# for that potential loop. It does this by concatinating the end of the loop with the
-# beginning of the loop. I take the FFT of this, normalize it (based on loop start FFT),
-# and calculate the distortion. I also make the lower frequencies more important using
-# the table `harmonic_bias`. This table punishes lower distortion more than higher distortion,
-# as the lower distortion is much more audible.
-
-# slice off the beginning and end of the sample. We don't want to deal with
-# the randomness of attack and release parts, because next we're looking for a
-# loop
-
-
-def calc_harmonics(x):
-    return abs(rfft(x))
-
-
-loop_search_slice = nontremmed[floor(attack_index):floor(release_index)]
-
-# slice_width is how wide of a slice to take from the beginning and end of the loop
-# align it to a power of 2 for FFT
-slice_width = 2 ** ceil(log2(max((sample_rate / freq), 512)))
-
-# ref_sample is the reference sample (for normalizing `test_loop`)
-ref_sample = loop_search_slice[0:slice_width]
-ref_sample_amps = resample_to(calc_harmonics(ref_sample), slice_width + 1)
-
-# this is a curve that biases the lower frequencies, making them more punishing
-harmonic_bias = (1 - (np.linspace(0.0, 1.0, slice_width + 1) ** 3)) * 2
-
-lowest_score = math.inf
-lowest_index = -1
-
-for i in np.arange(len(loop_search_slice) * 0.6, len(loop_search_slice) - slice_width, 2):
-    pos = floor(i)
-
-    # calculate score based on what provides the least harmonic distortion
-    potential_end = loop_search_slice[(pos - slice_width):pos]
-
-    # loop to test distortion
-    test_loop = np.concatenate((potential_end, ref_sample))
-    test_loop_amps = calc_harmonics(test_loop)
-
-    # normalize the amplitudes by the reference sample
-    test_loop_amps_norm = test_loop_amps / ref_sample_amps
-
-    # bias the lower frequencies, they are more audible
-    test_loop_amps_biased = np.maximum(test_loop_amps_norm, ref_sample_amps) * harmonic_bias
-
-    # calculate distortion (sqrt isn't necessary)
-    score = np.sum(np.abs(test_loop_amps_biased)**2)
-
-    if score < lowest_score:
-        lowest_score = score
-        lowest_index = pos
-
-loop_end = attack_index + lowest_index
+res = np.convolve(nontremmed[(loop_end_area - slice_width * 2):(loop_end_area + slice_width * 2)],
+                  nontremmed[(attack_index - slice_width):(attack_index + slice_width)], mode="valid")
+loop_end = np.argmax(res) + loop_end_area - slice_width * 2
 
 plt.plot(np.concatenate((nontremmed[(loop_end - slice_width):loop_end],
                          nontremmed[attack_index:(attack_index + slice_width)])))
 plt.show()
 
+# 274641
+
+
+def cosine_fadeout(x):
+    """ x: 0 - 1 """
+    return np.cos(x * math.pi / 2)
+
+
+def cosine_fadein(x):
+    return np.cos((x * math.pi / 2) - math.pi / 2)
+
+
 # loop test
-loop = nontremmed[attack_index:loop_end]
-out = np.concatenate((loop, loop, loop, loop, loop))
+loop = np.copy(nontremmed[attack_index:loop_end])
+
+crossfade_length = 512
+crossfade = np.linspace(0, 1, crossfade_length)
+fadeout = cosine_fadeout(crossfade)
+fadein = cosine_fadein(crossfade)
+
+crossed = (fadeout * loop[-crossfade_length:]) + (fadein * loop[0:crossfade_length])
+
+ang = np.angle(zoom_fft(res, [440, 440], fs=48000, m=1))[0]
+ang += (440*(1/48000)*crossfade_length)
+after_ang = np.angle(zoom_fft(loop[0:crossfade_length], [440, 440], fs=48000, m=1))[0]
+delta_ang = after_ang - ang+math.pi
+delta_ang_mapped = delta_ang/440*48000
+
+loop_sans_crossed = loop[crossfade_length:-crossfade_length]
+
+plt.plot(np.concatenate((loop_sans_crossed[-crossfade_length:], crossed, loop_sans_crossed[0:crossfade_length])))
+plt.axvline(x=crossfade_length, color="black")
+plt.axvline(x=crossfade_length * 2, color="black")
+plt.show()
+
+out = np.concatenate((loop_sans_crossed, crossed, loop_sans_crossed,
+                      crossed, loop_sans_crossed, crossed, loop_sans_crossed))
 wavfile.write("loop.wav", sample_rate, out)
 
 plt.plot(envelope_db)
